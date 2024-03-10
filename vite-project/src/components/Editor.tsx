5127c199ea561820eb374e01f0583d3619d4526a
JohnLTI <xjl16@outlook.com>
2024-03-10 01:21:49 -0300

    Creeate floating menu and editor tools

--- vite-project/src/components/Editor.tsx
+++ vite-project/src/components/Editor.tsx
@@ -1,7 +1,21 @@
-import { useEditor, EditorContent, BubbleMenu } from "@tiptap/react";
+import {
+  useEditor,
+  EditorContent,
+  BubbleMenu,
+  FloatingMenu,
+} from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { initialContent } from "./initialContent";
+import {
+  RxFontBold,
+  RxFontItalic,
+  RxStrikethrough,
+  RxCode,
+  RxChevronDown,
+  RxChatBubble,
+} from "react-icons/rx";
 import "highlight.js/styles/atom-one-dark.css";
+import { BubbleButton } from "./BubbleButton";
 
 export function Editor() {
   const editor = useEditor({
@@ -21,14 +35,70 @@ export function Editor() {
         editor={editor}
       />
       {editor && (
+        <FloatingMenu
+          editor={editor}
+          className="py-2 px-1 bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg 
+          overflow-hidden flex 
+          flex-col"
+          shouldShow={({ state }) => {
+            const { $from } = state.selection;
+            const currentLineText = $from.nodeBefore?.textContent;
+
+            return currentLineText === "/";
+          }}
+        >
+          <button className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600">
+            <img
+              src="http://www.notion.so/images/blocks/text/en-US.png"
+              alt="Text"
+            />
+            <div className="">
+              <span>Text</span>
+              <span>Just start writing with plain text.</span>
+            </div>
+          </button>
+        </FloatingMenu>
+      )}
+      {editor && (
         <BubbleMenu
           className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex 
           divide-x divide-zinc-600"
           editor={editor}
         >
-          <button className="">bold</button>
-          <button className="">bold</button>
-          <button className="">bold</button>
+          <BubbleButton>
+            Text
+            <RxChevronDown className="w-4 h-4 ml-1" />
+          </BubbleButton>
+          <BubbleButton>
+            <RxChatBubble className="w-4 h-4 mr-2" />
+            Comment
+          </BubbleButton>
+          <div className="flex items-center">
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              data-active={editor.isActive("bold")}
+            >
+              <RxFontBold className="w-4 h-4" />
+            </BubbleButton>
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              data-active={editor.isActive("strike")}
+            >
+              <RxStrikethrough className="w-4 h-4" />
+            </BubbleButton>
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              data-active={editor.isActive("italic")}
+            >
+              <RxFontItalic className="w-4 h-4" />
+            </BubbleButton>
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleCode().run()}
+              data-active={editor.isActive("code")}
+            >
+              <RxCode className="w-4 h-4" />
+            </BubbleButton>
+          </div>
         </BubbleMenu>
       )}
     </>
